6aa171ccd84be8ecba10ef80ad109ad74418acdb
Viacheslav Vasianovych <keebraa@gmail.com>
2019-08-27 12:57:02 +0300

    add fix for closed socket

--- src/main/java/com/keebraa/telegraph/remote/RemoteServiceResolver.java
+++ src/main/java/com/keebraa/telegraph/remote/RemoteServiceResolver.java
@@ -14,6 +14,7 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 import java.time.Instant;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
@@ -90,6 +91,8 @@ public class RemoteServiceResolver {
                     Socket clientSocket = serverSocket.accept();
                     executor.execute(() -> handleClientSocket(clientSocket));
                 }
+            } catch (SocketException exception) {
+                log.error("Multicast response socket was closed Resolved is stopped.");
             } catch (Exception exception) {
                 log.error("Server socket handling exception.", exception);
             }
